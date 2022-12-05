c810a078fd7e40175a5e226c236987fa46ae8950
Thomas <desplanthomas@gmail.com>
2022-12-05 12:11:05 +0100

    Classe Drawer and DrawerPlace

--- Entities/Drawer.cs
+++ Entities/Drawer.cs
@@ -1,6 +1,12 @@
-﻿namespace CaveManager.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace CaveManager.Entities
 {
+    [Table("Drawer")]
     public class Drawer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int IdCave { get; set; }
+        public List<DrawerPlace> DrawerPlaces {get; set;}
     }
 }
