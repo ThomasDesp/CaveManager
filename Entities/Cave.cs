671cd2a87eae13a9b4746b6f192b4d99fda38620
Melmanoucherie <119846185+Melmanoucherie@users.noreply.github.com>
2022-12-05 12:04:18 +0100

    Update Cave.cs

--- Entities/Cave.cs
+++ Entities/Cave.cs
@@ -2,5 +2,21 @@
 {
     public class Cave
     {
+        [Table("Cave")]
+    public class Cave
+    {
+
+        public Cave()
+        {
+
+        }
+
+
+        public int CaveID { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Drawer> Drawer { get; set; }
+    }
     }
 }
