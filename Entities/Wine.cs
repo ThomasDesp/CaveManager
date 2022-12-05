994b8a249a41800021bf9413abcfa1e4c8a90b78
lserrano63 <toushalo@hotmail.fr>
2022-12-05 11:41:17 +0100

    Wine class updated

--- Entities/Wine.cs
+++ Entities/Wine.cs
@@ -2,5 +2,16 @@
 {
     public class Wine
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Type { get; set; }
+        public string? Designation { get; set; }
+        public int? MinVintageRecommended { get; set; }
+        public int? MaxVintageRecommended { get; set; }
+        public int? IdDrawerPlace { get; set; }
+
+        public Wine()
+        {
+        }
     }
 }
