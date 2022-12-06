0e81359d89c327d6cbbe296e5599e51ccf036ab0
Melmanoucherie <laaaw159@gmail.com>
2022-12-06 10:40:56 +0100

    tytoffhfxx

--- Repository/CaveRepository.cs
+++ Repository/CaveRepository.cs
@@ -4,5 +4,64 @@ namespace CaveManager.Repository
 {
     public class CaveRepository : ICave
     {
+        CaveManagerContext context;
+        ILogger<CaveRepository> logger;
+        public CaveRepository(CaveManagerContext context, ILogger<CaveRepository> logger)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// Add an owner
+        /// </summary>
+        /// <param name="owner"></param>
+        /// <returns></returns>
+        public async Task<Cave> AddCaveAsync(Cave cave)
+        {
+            var addCave = context.Cave.Add(cave);
+            await context.SaveChangesAsync();
+            return owner;
+        }
+
+        /// <summary>
+        /// Get an owner by his id 
+        /// </summary>
+        /// <param name="idOwner"></param>
+        /// <returns></returns>
+        public async Task<Cave> SelectCaveAsync(int idCave)
+        {
+            return await context.Cave.FindAsync(idCave);
+        }
+
+        /// <summary>
+        /// Update an owner by his id
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public async Task<Cave> UpdateCaveAsync(int Id, string Name, int IdUser)
+        {
+            Cave caveUpdate = await context.Cave.FirstOrDefaultAsync(u => u.Id == idCave);
+            caveUpdate.Name = name;
+            caveUpdate.IdUser = idUser;
+            
+            //caveUpdate.
+
+            await context.SaveChangesAsync();
+            return caveUpdate;
+        }
+
+        /// <summary>
+        /// Remove an owner with his id
+        /// </summary>
+        /// <param name="idOwner"></param>
+        /// <returns></returns>
+        public async Task<bool> RemoveCaveAsync(int idCave)
+        {
+            var deleteCave = await context.Cave.Where(w => w.Id == idCave).SingleOrDefaultAsync();
+            context.Cave.Remove(deleteCave);
+            context.SaveChanges();
+            return true;
+        }
     }
 }
