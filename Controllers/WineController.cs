8fbad8791c48ced1d87ba2ea411a52249162bf55
lserrano63 <toushalo@hotmail.fr>
2022-12-06 16:41:48 +0100

    WineController added and configured

--- Controllers/WineController.cs
+++ Controllers/WineController.cs
@@ -1,4 +1,5 @@
-﻿using CaveManager.Repository;
+﻿using CaveManager.Entities;
+using CaveManager.Repository;
 using CaveManager.Repository.Repository.Contract;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,11 +10,78 @@ namespace CaveManager.Controllers
     public class WineController : ControllerBase
     {
         IWine wineRepository;
-        public WineController(IWine wineRepository)
+        private readonly ILogger<WineController> _logger;
+        public WineController(IWine wineRepository, ILogger<WineController> logger)
         {
             this.wineRepository = wineRepository;
+            _logger = logger;
         }
 
+        /// <summary>
+        /// Add a Wine to Drawer
+        /// </summary>
+        /// <param name="wine"></param>
+        /// <param name="idDrawer"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult<Wine>> AddWine(Wine wine, int idDrawer)
+        {
+            var wineAdded = await wineRepository.AddWineAsync(wine, idDrawer);
 
+            if (wineAdded != null)
+                return Ok(wineAdded);
+            else
+                return BadRequest("Wine is not added, please retry");
+        }
+
+        /// <summary>
+        /// Get wines from a drawer
+        /// </summary>
+        /// <param name="idDrawer"></param>
+        /// <returns></returns>
+        [HttpGet("{idDrawer}")]
+        public async Task<ActionResult<List<Wine>>> GetAllWinesFromADrawer(int idDrawer)
+        {
+            return Ok(await wineRepository.GetAllWinesFromADrawerAsync(idDrawer));
+        }
+
+        /// <summary>
+        /// Get a wine by his id
+        /// </summary>
+        /// <param name="idWine"></param>
+        /// <returns></returns>
+        [HttpGet("{idWine}")]
+        public async Task<ActionResult<Wine>> GetWine(int idWine)
+        {
+            return Ok(await wineRepository.GetWineAsync(idWine));
+        }
+
+        /// <summary>
+        /// Delete a wine
+        /// </summary>
+        /// <param name="idWine></param>
+        /// <returns></returns>
+        [HttpDelete("{idWine}")]
+        public async Task<ActionResult<bool>> DeleteWine(int idWine)
+        {
+            await wineRepository.DeleteWineAsync(idWine);
+            return Ok(true);
+        }
+
+        /// <summary>
+        /// Modify a wine
+        /// </summary>
+        /// <param name="idWine"></param>
+        /// <param name="name"></param>
+        /// <param name="type"></param>
+        /// <param name="designation"></param>
+        /// <param name="minVintageRecommended"></param>
+        /// <param name="maxVintageRecommended"></param>
+        /// <returns></returns>
+        [HttpPut("{idWine}")]
+        public async Task<ActionResult<Wine>> PutWine(int idWine, string name, string type, string designation, int minVintageRecommended, int maxVintageRecommended)
+        {
+            return Ok(await wineRepository.PutWineAsync(idWine,name,type,designation,minVintageRecommended, maxVintageRecommended));
+        }
     }
 }
