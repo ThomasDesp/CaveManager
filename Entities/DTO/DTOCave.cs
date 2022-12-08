764ce0aae5a4702f2d99204c887e5dda70b27d04
Melmanoucherie <laaaw159@gmail.com>
2022-12-08 09:14:52 +0100

    DTO Created

--- Entities/DTO/DTOCave.cs
+++ Entities/DTO/DTOCave.cs
@@ -2,5 +2,7 @@
 {
     public class DTOCave
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- Entities/DTO/DTOWine.cs
+++ Entities/DTO/DTOWine.cs
@@ -2,5 +2,11 @@
 {
     public class DTOWine
     {
+        public int IdWine { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string? Designation { get; set; }
+        public int? MinVintageRecommended { get; set; }
+        public int? MaxVintageRecommended { get; set; }
     }
 }
